4a76f9dc2eb420ae66b76626d6ee30e8e799d0f8
Christophe '116' Loiseau <116@lab0.net>
2018-03-18 10:30:34 +0100

    index on (no branch): 264d631 Use native Gradle support to run tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,50 +4,64 @@ val junitJupiterVersion = "5.1.0"
 val springBootVersion = "2.0.0.RELEASE"
 
 plugins {
-    val kotlinVersion = "1.2.30"
-    id("org.jetbrains.kotlin.jvm") version kotlinVersion
-    java
-    idea
+  val kotlinVersion = "1.2.30"
+  id("org.jetbrains.kotlin.jvm") version kotlinVersion
+  java
+  idea
 }
 
 idea {
-    module {
-        isDownloadSources = true
-    }
+  module {
+    isDownloadSources = true
+  }
 }
 
 repositories {
-    mavenCentral()
-    jcenter()
+  mavenCentral()
+  jcenter()
 }
 
 version = "0.1.0-SNAPSHOT"
 
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compile("org.jetbrains.kotlin:kotlin-reflect")
-    compile("org.springframework.boot:spring-boot-starter-parent:$springBootVersion")
-    compile("org.springframework.shell:spring-shell-starter:$springBootVersion")
+  compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  compile("org.jetbrains.kotlin:kotlin-reflect")
+  compile("org.springframework.boot:spring-boot-starter-parent:$springBootVersion")
+  compile("org.springframework.shell:spring-shell-starter:$springBootVersion")
 
-    testImplementation("org.assertj:assertj-core:$assertJVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion") {
-        exclude("junit:junit")
-    }
+  testImplementation("org.assertj:assertj-core:$assertJVersion")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+  testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion") {
+    exclude("junit:junit")
+  }
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
 buildscript {
-    repositories {
-        mavenCentral()
-        jcenter()
-    }
-    dependencies {
-        classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.+")
-    }
+  repositories {
+    mavenCentral()
+    jcenter()
+  }
+  dependencies {
+    classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.+")
+  }
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
+
+tasks.withType<Jar> {
+  configurations["compileClasspath"].forEach { file: File ->
+    from(zipTree(file.absoluteFile))
+  }
+  manifest {
+    attributes(
+        mapOf(
+            "Main-Class" to "net.lab0.shell.Application"
+        )
+    )
+  }
+}
+
